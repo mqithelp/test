965f0e6acb3f0dd6ec0653db138bfe2b4505637d
mqWork <mqithelp@gmail.com>
2024-06-03 15:58:39 +0300

    Task 1 BEGIN!

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-
+        System.out.println("Task 1");
     }
 }
