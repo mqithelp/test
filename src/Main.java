2c454a7a565fa0ae4c0384d033d0b7e157908cc8
MaxHome <mqithelp@gmail.com>
2024-07-18 11:29:12 +0300

    тут всё просто. щас будем усложнять

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,10 @@ public class Main {
         Switcher switcher = new Switcher();
         Lamp lamp = new Lamp();
         Radio radio = new Radio();
+//        switcher.consumer = lamp;
+        // event subscribe подписка на событие
         switcher.consumer = lamp;
+        switcher.switchOn();
         switcher.switchOn();
     }
 }
