a41dc66dd20633bb1e7962fde4f9048edc413e52
MaxHome <mqithelp@gmail.com>
2024-07-19 13:44:41 +0300

    Используем декларативный код. Использование стримов при работе с коллекциями

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,7 @@
 public class Main {
-    public static void fire(Object sender) {
-        System.out.println("Вызов из обчного статического метода.");
-    }
 
     public static void main(String[] args) {
-        Switcher switcher = new Switcher();
-        Lamp lamp = new Lamp();
-        Radio radio = new Radio();
-        // event subscribe подписка на событие
 
-        class Fire implements ElectricityConsumer {
-            @Override
-            public void electricityOn(Object sender) {
-                System.out.println("Пожар! В обычном классе!");
-            }
-        }
-        switcher.addElectricityListiner(radio);
-        switcher.addElectricityListiner(lamp);
-        switcher.addElectricityListiner(new Fire());
-        switcher.addElectricityListiner(
-                new ElectricityConsumer() {
-                    @Override
-                    public void electricityOn(Object sender) {
-                        System.out.println("Fire! Анонимный класс!");
-                    }
-                }
-        );
-
-        final String message = "Пожарище. Переменную передаём в лямбда выражение";
-
-        switcher.addElectricityListiner(
-                sender -> System.out.println("FIRE! В лямбда-выражении!")
-        );
-        //захват переменных
-        switcher.addElectricityListiner( sender -> System.out.println(message) );
-
-        switcher.addElectricityListiner(sender -> Main.fire(sender));
-        switcher.addElectricityListiner(Main::fire);
-        switcher.switchOn();
 
     }
 }
