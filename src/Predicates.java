faa075749ed693fdc4910ac3a16d4bd08ad0fc80
MaxHome <mqithelp@gmail.com>
2024-07-20 01:30:48 +0300

    работа лямбда-выражений с коллекциями

--- src/Predicates.java
+++ src/Predicates.java
@@ -10,9 +10,15 @@ public class Predicates {
         persons.add(new Person("Глаша", 3));
         persons.add(new Person("Саша", 6));
         persons.add(new Person("Анна", 18));
-        for (Person p : persons ) {
-            System.out.println(p);
-        }
-
+//        for (Person p : persons ) {
+//            System.out.println(p);
+//        }
+        persons.stream().forEach(p -> System.out.println(p));
+        persons.stream().forEach(System.out::println);
+        System.out.println("Выведем коллекция \"взрослых\" т.е. отфильтруем значения");
+        persons.stream().
+                filter(p -> p.getAge() >=18).
+                sorted((p1,p2) -> p1.getName().compareTo(p2.getName())).
+                forEach(System.out::println);
     }
 }
